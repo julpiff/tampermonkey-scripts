220f44c1e3745c733a2621dc59af472519740415
julpiff <167293030+julpiff@users.noreply.github.com>
2025-04-14 09:16:05 +0200

    Update Launchpad.user.js

--- Launchpad.user.js
+++ Launchpad.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Launchpad
 // @namespace    http://tampermonkey.net/
-// @version      2.8
+// @version      2.9
 // @description  Displays a sleek Launchpad-style modal with specific buttons, each triggering custom events.
 // @author       julpif
 // @match        https://mad.ingrid.com/*
