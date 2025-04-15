6ac2437bd2c717b03c517da83c232d7afe05e933
julpiff <167293030+julpiff@users.noreply.github.com>
2025-04-15 08:53:30 +0200

    Update Stage environment color header.user.js

--- Stage environment color header.user.js
+++ Stage environment color header.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Stage environment color header
 // @namespace    http://tampermonkey.net/
-// @version      1.6
+// @version      1.7
 // @description  Style the header and primary button on the staging environment
 // @author       julpif
 // @match        https://mad.ingrid.com/*
