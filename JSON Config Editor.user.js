c0810fe3663c49811965641b03516428ebeebeaf
julpiff <167293030+julpiff@users.noreply.github.com>
2025-04-15 08:52:54 +0200

    Update JSON Config Editor.user.js

--- JSON Config Editor.user.js
+++ JSON Config Editor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JSON Config Editor
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      2.1
 // @description  Enhanced JSON configuration editor
 // @author       julpif
 // @match        https://mad.ingrid.com/*
